0dae6c5bb9ffa912ce354dce65e6d245e9c3da78
HaciIsma <haciismayilov5@outlook.com>
2019-12-14 16:56:06 +0400

    first commit

---
+++ Model/Storage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup
+{
+    abstract class Storage
+    {
+        public string mediaName { get; set; }
+        public string model { get; set; }
+        public abstract void Copy();
+        public abstract decimal Capasity();
+        public abstract decimal FreeMemory();
+        public abstract void PrintDeviceİnfo();
+    }
+}
---
+++ Model/Storages/DVD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup
+{
+    class DVD : Storage
+    {
+        public override decimal Capasity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Copy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal FreeMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PrintDeviceİnfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

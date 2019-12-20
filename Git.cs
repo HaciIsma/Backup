e5871585d5b37590bcb67e6ced6c3eee0398a1fe
HaciIsma <haciismayilov5@outlook.com>
2019-12-20 12:33:59 +0400

    12

--- Git.cs
+++ Git.cs
@@ -13,8 +13,8 @@ namespace Backup
             Console.Write("Enter commit: ");
             commit = Console.ReadLine();
             commit = "\'" + commit + "\'";
-            string remoteControl = "https://github.com/HaciIsma/Backup.git";
-            string command = $"cd..; cd..; git init; git commit -m {commit};git add .;git remote add origin {remoteControl} ;git push -u origin master;pause";
+            string remoteControl = "https://github.com/HaciIsma/Git.git";
+            string command = $"cd..; cd..; git init;git add .; git commit -m {commit};git remote add origin {remoteControl} ;git push -u origin master;pause";
             process.StartInfo = new ProcessStartInfo(destination, command);
             process.Start();
         }
